c02aa3ca0f6fb49fdd9f9d8bcd13ded3c08fccb7
ricodroid404 <ricodroid2024@gmail.com>
2024-02-17 15:33:16 +0900

    databindingを設定

--- app/src/main/java/com/example/android_motto_miru_button/MainActivity.kt
+++ app/src/main/java/com/example/android_motto_miru_button/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.android_motto_miru_button
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.android_motto_miru_button.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.textView1.text = "もっと見るボタンを作成していく〜〜🚀"
     }
 }
